2574fbfbed7ab94c0691c6048851b7a5cdc8193f
Alex Panov <thealexpanov@gmail.com>
2016-11-01 17:00:39 -0400

    Fix test name

--- src/components/test/Button.test.js
+++ src/components/test/Button.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import {shallow} from "enzyme";
 import Button from "../Button";
 
-describe('<CenteredHeading />', () => {
+describe('<Button />', () => {
   it('should contain text', () => {
     const wrapper = shallow(<Button>Hello</Button>);
     expect(wrapper.text()).to.equal('Hello');
